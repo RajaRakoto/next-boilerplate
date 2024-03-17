ca78d39555829a7ed071f1e00b6090bd1fbd2e91
raja <raja.rakoto7@gmail.com>
2024-03-17 21:18:55 +0300

    hotfix

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,10 +1,11 @@
-import type { Config } from 'tailwindcss';
+import type { Config } from "tailwindcss";
 
 const config: Config = {
-	content: ['./src/**/*.{js,ts,jsx,tsx}'],
+	content: ["./src/**/*.{js,ts,jsx,tsx}"],
 	theme: {
 		extend: {},
 	},
 	plugins: [],
 };
+
 export default config;
