fae3e66d93cb077e96eb7ce536ed7dc9f4e5bf1f
raja <raja.rakoto7@gmail.com>
2024-03-23 12:19:39 +0300

    reorganize

--- e2e/main.spec.ts
+++ e2e/main.spec.ts
@@ -3,16 +3,16 @@ import { test, expect } from "@playwright/test";
 test("has title", async ({ page }) => {
 	await page.goto("https://playwright.dev/");
 
-	// Expect a title "to contain" a substring.
+	// expect a title "to contain" a substring.
 	await expect(page).toHaveTitle(/Playwright/);
 });
 
 test("get started link", async ({ page }) => {
 	await page.goto("https://playwright.dev/");
 
-	// Click the get started link.
+	// click the get started link.
 	await page.getByRole("link", { name: "Get started" }).click();
 
-	// Expects the URL to contain intro.
+	// expects the URL to contain intro.
 	await expect(page).toHaveURL(/.*intro/);
 });
