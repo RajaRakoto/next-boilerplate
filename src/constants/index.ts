2466c5ae8ff853c3932c225271fa221288d0cc1c
raja <raja.rakoto7@gmail.com>
2024-03-23 17:12:38 +0300

    src: add logos -> constants

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,13 +1,5 @@
-// colors
-export const PRIMARY_COLOR = "#007bff";
-export const SECONDARY_COLOR = "#6c757d";
-
-// font size
-export const SMALL_FONT_SIZE = "12px";
-export const MEDIUM_FONT_SIZE = "16px";
-export const LARGE_FONT_SIZE = "24px";
-
-// api
-export const BASE_URL = "https://api.example.com";
-export const USERS_ENDPOINT = "/users";
-export const POSTS_ENDPOINT = "/posts";
+// images
+export const bunLogo = "/images/bun.svg";
+export const nextLogo = "/images/next.svg";
+export const turbopackLogo = "/images/turbopack.svg";
+export const vercelLogo = "/images/vercel.svg";
