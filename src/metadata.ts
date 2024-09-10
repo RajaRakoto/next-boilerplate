496f1ca2ccc50da3f79f37492274cb419af89704
raja <raja.rakoto7@gmail.com>
2024-09-11 00:29:39 +0300

    set boilerplate description

--- src/metadata.ts
+++ src/metadata.ts
@@ -3,7 +3,7 @@ import type { Metadata } from "next";
 export const metadata: Metadata = {
 	title: "Next Boilerplate 🚀",
 	description:
-		"Minimalistic boilerplate to quick-start Next.js development in TypeScript, optimized for blazing-fast performance Bun & Turbopack 🚀",
+		"Minimalistic fullstack boilerplate to quick-start Next.js development in TypeScript, optimized for blazing-fast performance Bun & Turbopack 🚀",
 	keywords: "typescript, turbopack, next, bun",
 	authors: [{ name: "Raja Rakotonirina", url: "" }],
 };
